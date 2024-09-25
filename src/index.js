98a3ffaa306f01829eb7ba720c93aa3ef516fc6b
ukioxz <ikatember@gmail.com>
2024-09-25 23:12:06 +0300

    Add server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import * as PIXI from "pixi.js";
 //import Game from "./game.js";
 
-const app = new PIXI.Application();
+/*const app = new PIXI.Application();
 await app.init({ width: 640, height: 360 });
 document.body.appendChild(app.canvas);
 
 await PIXI.Assets.load("../public/sprites/1 Apple.png");
 let sprite = PIXI.Sprite.from("../public/sprites/1 Apple.png");
-app.stage.addChild(sprite);
+app.stage.addChild(sprite);*/
+const app = new PIXI.Application();
+/*const app = new PIXI.Application({
+  width: 640,
+  height: 360,
+  backgroundColor: 0x1099bb, // Задаємо фон, наприклад, синій
+});*/
+await app.init({ width: 640, height: 360 });
+document.body.appendChild(app.canvas); // Додаємо canvas на сторінку
+
+// Завантажуємо текстуру і після цього додаємо спрайт
+PIXI.Assets.load("../public/sprites/1 Apple.png").then(() => {
+  let sprite = PIXI.Sprite.from("../public/sprites/1 Apple.png");
+  sprite.x = app.renderer.width / 2;
+  sprite.y = app.renderer.height / 2;
+  sprite.anchor.set(0.5); // Центруємо спрайт
+  app.stage.addChild(sprite);
+});
